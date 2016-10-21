43ec0865a0c47e967c8684c649da2df57ca0bdd5
tylerlaberge <tyler.laberge@maine.edu>
2016-10-21 15:34:30 -0400

    created service class for handling model creation and insertion into database.

--- sensor.py
+++ sensor.py
@@ -1,37 +1,30 @@
 import time
 from scapy.all import sniff, IP, TCP
-from models import CyberAttack
-from dbsession import DBSession
- 
+from services import CyberAttackService
+
+
+class Sensor(object):
+
+    def __init__(self, callback, iface='enp0s8', store=0):
+        self.iface = iface
+        self.store = store
+        self.callback = callback
+
+    def start_sniffing(self):
+        sniff(iface=self.iface, store=self.store, prn=self.callback)
+
+
+service = CyberAttackService()
+
 
 def pcap_default(packet):
-    if packet.haslayer(IP) and packet.haslayer(TCP): 
-        db_session = DBSession()
+    if packet.haslayer(IP) and packet.haslayer(TCP):
         ip = packet[IP]
         tcp = packet[TCP]
 
-        cyber_attack = CyberAttack(source_ip=ip.src, dest_ip=ip.dst,
-                                   source_port=tcp.sport, dest_port=tcp.dport,
-                                   protocol=ip.proto, time=time.time())
-        db_session.db_connect()
-        db_session.db_update(cyber_attack)
+        service.create_model(source_ip=ip.src, dest_ip=ip.dst,
+                             source_port=tcp.sport, dest_port=tcp.dport,
+                             protocol=ip.proto, time=time.time())
 
-
-class Sensor():
-    iface = None
-    store = None
-    prn = None
-
-
-    def start_sniffing(self):
-        sniff(iface=self.iface, store=self.store, prn=self.prn)
-
-    
-    def __init__(self, iface='enp0s8', store=0, prn=lambda packet: pcap_default(packet)):
-        self.iface = iface
-        self.store = store
-        self.prn = prn
-
-
-sens = Sensor()
+sens = Sensor(callback=lambda packet: pcap_default(packet))
 sens.start_sniffing()
---
+++ services.py
@@ -0,0 +1,22 @@
+from dbsession import DBSession
+from models import CyberAttack
+
+
+class Service(object):
+
+    def __init__(self, model):
+        self.db_session = DBSession()
+        self.db_session.db_connect()
+        self.model = model
+
+    def create_model(self, **kwargs):
+        model = self.model(**kwargs)
+        self.db_session.db_update(model)
+
+        return model
+
+
+class CyberAttackService(Service):
+
+    def __init__(self):
+        super().__init__(CyberAttack)
