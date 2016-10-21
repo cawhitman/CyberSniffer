940713dfcc9e6bf6b4b987fa485a909f32b8a43c
tylerlaberge <tyler.laberge@maine.edu>
2016-10-21 17:03:02 -0400

    removed sensor class, unnecessary wrapper for the sniff method. added BPF syntax filter to sniff method.

--- sensor.py
+++ sensor.py
@@ -2,19 +2,26 @@ import time
 from scapy.all import sniff, IP, TCP
 from services import CyberAttackService
 
-
+"""
 class Sensor(object):
 
-    def __init__(self, callback, iface='enp0s8', store=0):
+    def __init__(self, callback, filter=None, iface='enp0s8', store=0):
+        self.callback = callback
         self.iface = iface
         self.store = store
-        self.callback = callback
+        self.filter = filter
 
     def start_sniffing(self):
         sniff(iface=self.iface, store=self.store, prn=self.callback)
 
+    def handle_packet(self, packet):
+        if packet.haslayer(IP) and packet.haslayer(TCP):
+            ip = packet[IP]
+            tcp = packet[TCP]
 
-service = CyberAttackService()
+
+
+
 
 
 def pcap_default(packet):
@@ -27,5 +34,15 @@ def pcap_default(packet):
                                  source_port=tcp.sport, dest_port=tcp.dport,
                                  protocol=ip.proto, time=time.time())
 
-sens = Sensor(callback=lambda packet: pcap_default(packet))
-sens.start_sniffing()
+"""
+
+cyber_attack_service = CyberAttackService()
+sniff(
+    iface='enp0s8', store=0,
+    filter="proto ip and proto tcp and dst host 192.168.56.103",
+    prn=lambda packet: cyber_attack_service.create_model(
+        source_ip=packet[IP].src, dest_ip=packet[IP].dst,
+        source_port=packet[TCP].sport, dest_port=packet[TCP].dport,
+        protocol=packet[IP].proto, time=time.time()
+    )
+)
